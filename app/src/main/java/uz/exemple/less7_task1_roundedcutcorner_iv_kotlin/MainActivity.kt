fc9f8692c4b92c75adcea591ac0258db845a0adf
Khushnud2005 <bayhushnud@gmail.com>
2022-08-26 09:07:32 +0500

    Initial commit

---
+++ app/src/main/java/uz/exemple/less7_task1_roundedcutcorner_iv_kotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package uz.exemple.less7_task1_roundedcutcorner_iv_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
